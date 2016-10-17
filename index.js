5bc14134b80534cc8dd6f65f98f4ec9e4fab7ad3
jmo <jason.olson@mx.com>
2016-10-17 11:43:52 -0600

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const credentials = require('./credentials.json');
-const Atrium = require('mx-atrium');
+const Atrium = require('../atrium');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const credentials = require('./credentials.json');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const AtriumClient = new Atrium.Client(credentials.apiKiey, credentials.clientId, Atrium.environments.sand);
 
